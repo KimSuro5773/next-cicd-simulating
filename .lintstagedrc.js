db92e9ec2f4cd627af7cd91137dc3dbbdafe6ea1
KimSuro <kimsuro5773@gmail.com>
2026-03-11 15:54:10 +0900

    테스트10

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,12 +1,16 @@
 import { relative } from "path";
 
 const buildEslintCommand = (filenames) =>
-  `eslint --fix ${filenames
-    .map((f) => `"${relative(process.cwd(), f)}"`)
-    .join(" ")}`;
+  `eslint --fix ${filenames.map((f) => `"${relative(process.cwd(), f)}"`).join(" ")}`;
 
 const config = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand],
+  "*.{js,jsx,ts,tsx}": [
+    "prettier --write", // 변경된 파일만 포맷
+    buildEslintCommand, // 변경된 파일만 lint
+  ],
+  "*.{css,md,json}": [
+    "prettier --write", // 기타 파일 포맷
+  ],
 };
 
 export default config;
